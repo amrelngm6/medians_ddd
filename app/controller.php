ca366efbe0c2d7338959fc7928d58d5a3b88ffa0
Amr <amr@cg.sa>
2022-11-27 06:19:20 +0200

    Some Enhancements

--- app/controller.php
+++ app/controller.php
@@ -190,9 +190,28 @@ $app->match('facebook_login_back', function () use ($twig, $request, $app)
 $app->match('fb/webhook', function () use ($twig, $request, $app) 
 {
 
-    error_log(json_encode($_GET));
-    error_log(json_encode($_REQUEST));
+    $config = (new apps\Auth\AuthService(new UserRepository))->fbConfigQuery();
 
+
+    $verifyToken = '1010'; // You will specify it when you enable the Webhook for your app
+    $appSecret = $config->api_secret;
+
+    // Handle verification request
+    if (isset($_GET['hub_mode']) && $_GET['hub_mode'] === 'subscribe') {
+        if ($_GET['hub_verify_token'] === $verifyToken) {
+            echo $_GET['hub_challenge'];
+            exit;
+        }
+    }
+
+    // Validate the integrity and payload and it's origin
+    $payload = file_get_contents('php://input');
+    if (isset($_SERVER['HTTP_X_HUB_SIGNATURE']) && hash_equals(explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE'])[1], hash_hmac('sha1', $payload, $appSecret))) {
+        // Handle payload
+        $data = json_decode($payload, true);
+        file_put_contents(time().'-webhook.php', $payload);
+        exit;
+    }
 
     if (!empty($request->get('hub_challenge')))
     {
