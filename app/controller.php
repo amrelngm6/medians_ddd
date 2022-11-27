1bddf435e8d610edcdad82306bc590cbdf6e2d43
Amr <amr@cg.sa>
2022-11-27 04:11:28 +0200

    Some Enhancements

--- app/controller.php
+++ app/controller.php
@@ -189,6 +189,8 @@ $app->match('facebook_login_back', function () use ($twig, $request, $app)
 */
 $app->match('fb/webhook', function () use ($twig, $request, $app) 
 {
+    error_log(json_encode($_GET));
+    error_log(json_decode($_POST));
     return $request->get('hub_challenge');
 });
 
