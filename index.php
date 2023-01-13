d2258ca1e7f1d929963a1ca8aaab4482cf61fed5
Amr <amr@cg.sa>
2023-01-13 10:47:26 +0200

    Fix issue

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-session_start(); error_reporting(E_ALL);
+session_start(); error_reporting(E_ERROR | E_WARNING | E_PARSE);
 date_default_timezone_set('Africa/Cairo');
 
 require_once __DIR__.'/app/config/database.php';
